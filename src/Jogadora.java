7b656ccee2796f81d376c3fa443ac2f0cc10b347
Jean <jeanmatheus0612@gmail.com>
2025-04-16 20:14:52 -0300

    Adicionando os métodos getters e setters

--- src/Jogadora.java
+++ src/Jogadora.java
@@ -15,4 +15,36 @@ public class Jogadora implements OperacaoCadastro{
     public void cadastrar() {
         System.out.println("A jogadora " + nome + "foi cadastrada com sucesso!");
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getPosicao() {
+        return posicao;
+    }
+
+    public void setPosicao(String posicao) {
+        this.posicao = posicao;
+    }
+
+    public int getNumeroCamisa() {
+        return numeroCamisa;
+    }
+
+    public void setNumeroCamisa(int numeroCamisa) {
+        this.numeroCamisa = numeroCamisa;
+    }
 }
